973b84960e0999c9b3b81f6557c84d69c15a9eaf
satansama6 <30526720+satansama6@users.noreply.github.com>
2017-11-24 14:44:03 +0200

    f
    f

---
+++ Assets/BacisMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BacisMovement : MonoBehaviour
+{
+  // This only works with the mouse but you should be able to figure out from here
+
+  public Camera mainCamera;
+
+  public float m_MovementSpeed = 50;
+  public float m_HorizontalRotationSpeed = 50;
+  public float m_VerticalRotationSpeed = 20;
+
+  private float m_Yaw = 0;
+  private float m_Pitch = 0;
+
+  // Update is called once per frame
+  private void Update()
+  {
+    if (Input.GetKey(KeyCode.W))
+    {
+      transform.position += transform.forward * m_MovementSpeed * Time.deltaTime;
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+      transform.position -= transform.forward * m_MovementSpeed * Time.deltaTime;
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      transform.position += transform.right * m_MovementSpeed * Time.deltaTime;
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      transform.position -= transform.right * m_MovementSpeed * Time.deltaTime;
+    }
+
+    m_Yaw += m_HorizontalRotationSpeed * Input.GetAxis("Mouse X");
+    m_Pitch -= m_VerticalRotationSpeed * Input.GetAxis("Mouse Y");
+    transform.eulerAngles = new Vector3(0, m_Yaw, 0);
+    mainCamera.transform.eulerAngles = new Vector3(m_Pitch, m_Yaw, 0);
+  }
+}
---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+  public Transform target;
+  private Vector3 offset;
+
+  // Use this for initialization
+  private void Start()
+  {
+    // Get the offset from the cameras position relative to the target
+    offset = target.position - transform.position;
+  }
+
+  // We use late update so the camera always moves after the target moved, otherwise we would get some jitterish movement
+  private void LateUpdate()
+  {
+    transform.position = target.position + offset;
+  }
+}
